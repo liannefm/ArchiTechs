3995b41a32142a1a9795ca47da1dde76a1fc600d
NoaPhilene <philenevanmil@gmail.com>
2025-11-25 11:45:26 +0100

    sidebar bijna klaar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,29 +1,26 @@
 <body>
 
 <!-- Sidebar -->
-<div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
+<div class="mySidebar" style="display:none" id="mySidebar">
     <div id="sidebar">
-        <div id="sidebarbox">
-    <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
-        <!-- hier komen de knoppen -->
+        <div id="sidebar_box">
+            <button onclick="sidebar_close()" class="close_button">&times;</button>
+            <!-- hier komen de knoppen -->
         </div>
     </div>
 </div>
 
 <!-- Page Content -->
-<div class="w3-teal">
-  <button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">☰</button>
-  <div class="w3-container">
-    <h1>My Page</h1>
-  </div>
+<div class="hamburger_box">
+  <button class="hamburger_menu" onclick="sidebar_open()">☰</button>
 </div>
 
 <script>
-function w3_open() {
+function sidebar_open() {
   document.getElementById("mySidebar").style.display = "block";
 }
 
-function w3_close() {
+function sidebar_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
 </script>    
