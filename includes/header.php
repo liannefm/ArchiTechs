96ddfbd8050aa22e58f6b5cff18f71de766fa76e
liannefm <lfmeiresonne@gmail.com>
2025-11-27 10:52:48 +0100

    favicon

--- includes/header.php
+++ includes/header.php
@@ -8,4 +8,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>HUA</title>
+
+    <link rel="icon" type="image/png" rel="noopener" targer="_blank" href="includes/image/hualogo.png">
 </head>
